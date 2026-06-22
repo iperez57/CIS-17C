e1ab01fa0572273cb9a3ce85a269038a804d3743
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-06-22 09:07:12 -0700

    outline for assignment 0 part b

---
+++ Assignment_0_PtB/Assignment_0_PtB/Assignment_0_PtB.cpp
@@ -0,0 +1,23 @@
+// Assignment_0_PtB.cpp
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void readPrices(ifstream&, int[]);
+void displayPrices(int[], int);
+
+int main()
+{
+	const int size = 15;
+	int arr[size];
+	ifstream file("prices.txt");
+
+	if (!file)
+	{
+		cout << "Error opening file." << endl;
+		return 0;
+	}
+
+}
