63b25a3cd2f7e03abba243d0ca8c914e3861c72a
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-06-22 16:50:31 -0700

    outline for assignment 1 part c

---
+++ Assignment_1_PtC/Assignment_1_PtC/Assignment_1_PtC.cpp
@@ -0,0 +1,44 @@
+// Assignment_1_PtC.cpp
+
+/* FIX
+* #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+void hypotenuse(double leg1, double leg2, double* resultPtr) 
+{
+	*resultPtr = sqrt(leg1 * leg1 + leg2 * leg2);
+}
+
+int main() 
+{
+
+	double* p;
+
+	hypotenuse(1.5, 2.0, p);
+
+	cout << "The hypotenuse is " << *p << endl;
+
+}
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+void hypotenuse(double leg1, double leg2, double* resultPtr)
+{
+	*resultPtr = sqrt(leg1 * leg1 + leg2 * leg2);
+}
+
+
+int main() {
+	double* p;
+
+	hypotenuse(1.5, 2.0, p);
+	cout << "The hypotenuse is " << *p << endl;
+}
