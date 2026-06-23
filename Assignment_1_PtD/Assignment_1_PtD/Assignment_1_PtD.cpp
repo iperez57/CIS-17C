7a8df6297a3cb30fd102a210cd8fcfe0f0717670
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-06-23 10:33:36 -0700

    outline for assignment 1 pt d

---
+++ Assignment_1_PtD/Assignment_1_PtD/Assignment_1_PtD.cpp
@@ -0,0 +1,59 @@
+// Assignment_1_PtD.cpp
+
+/* FIX
+// return true if two C strings are equal
+bool match(const char str1[], const char str2[]) {
+while (str1 != 0 && str2 != 0) { // zero bytes at ends
+if (str1 != str2) // compare corresponding chars
+return false;
+str1++; // advance to the next character
+str2++;
+}
+return str1 == str2; // both ended at same time?
+}
+
+
+int main()
+{
+char a[10] = "pointy";
+char b[10] = "pointless";
+if (match(a,b))
+cout << "They're the same!\n";
+else
+        cout << “They’re different!\n”;
+return 0;
+
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// return true if two C strings are equal
+bool match(const char str1[], const char str2[]) 
+{
+    while (str1 != 0 && str2 != 0) { // zero bytes at ends
+        if (str1 != str2) // compare corresponding chars
+            return false;
+
+        str1++; // advance to the next character
+        str2++;
+    }
+    return str1 == str2; // both ended at same time?
+}
+
+
+int main() 
+{
+    char a[10] = "pointy";
+    char b[10] = "pointless";
+
+    if (match(a, b))
+        cout << "They're the same!\n";
+    else
+        cout << "They’re different!\n";
+
+    return 0;
+
+}
