d534e44931d1e403f0ddb33db23421ba00a7b270
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-06-25 09:38:16 -0700

    deletes the s and S in a string

--- Assignment_1_PtE/Assignment_1_PtE/Assignment_1_PtE.cpp
+++ Assignment_1_PtE/Assignment_1_PtE/Assignment_1_PtE.cpp
@@ -6,12 +6,25 @@ using namespace std;
 
 void deleteS(char* c)
 {
-    char* s;
+    char* s = c;
 
+    while (*c != '\0')
+    {
+        if (*c != 's' && *c != 'S')
+        {
+            *s = *c;
+            s++;
+        }
+        c++;
+    }
+    *s = '\0';
 }
 
 
 int main()
 {
+    char sent[] = "So whats the weather like";
+    deleteS(sent);
 
+    cout << sent << endl;
 }
