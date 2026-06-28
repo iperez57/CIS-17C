59a5b936b290932ea091a031b05b561e0d545254
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-06-28 08:30:31 -0700

    implemented all pet class functions

--- Assignment_2/Assignment_2/Pet.cpp
+++ Assignment_2/Assignment_2/Pet.cpp
@@ -3,34 +3,48 @@
 #include "Pet.h"
 
 // Initialize the state of the pet
-Pet::Pet(string nm, int initialHealth) {
+Pet::Pet(string nm, int initialHealth) 
+{
     m_name = nm;
     m_health = initialHealth;
 }
 
-void Pet::eat(int amt) {
+void Pet::eat(int amt) 
+{
     // TODO: Increase the pet's health by the amount
+    m_health += amt;
 }
 
-void Pet::play() {
+void Pet::play() 
+{
     // TODO: Decrease pet's health by 1 for the energy consumed
+    m_health -= 1;
+
 }
 
-string Pet::name() const {
+string Pet::name() const 
+{
     // TODO: Return the pet's name.  Delete the following line
     // and replace it with the correct code.
-    return ""; // This implementation compiles, but is incorrect
+    return m_name;
 }
 
-int Pet::health() const {
+int Pet::health() const 
+{
     // TODO: Return the pet's current health level.  Delete the
     // following line and replace it with the correct code.
-    return 99; // This implementation compiles, but is incorrect
+    return m_health;
 }
 
-bool Pet::alive() const {
+bool Pet::alive() const 
+{
     // TODO: Return whether pet is alive.  (A pet is alive if
     // its health is greater than zero.)  Delete the following
     // line and replace it with the correct code.
-    return true; // This implementation compiles, but isn’t right
+    if (m_health > 0)
+    {
+        return true;
+    }
+    else
+    return false; 
 }
