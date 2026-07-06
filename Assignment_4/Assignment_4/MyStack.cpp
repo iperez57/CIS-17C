d38dc38c748354d43dcb88c1127369430440c3d2
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-07-06 12:38:46 -0700

    constructors for linked list are defined

---
+++ Assignment_4/Assignment_4/MyStack.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Node.h"
+#include "MyStack.h"
+
+using namespace std;
+
+MyStack::MyStack()
+{
+	dummy.next = nullptr;
+	dummy.value = 0;
+}
+
+MyStack::MyStack(const MyStack& rhs)
+{
+	dummy.value = rhs.dummy.value;
+	dummy.next = nullptr;
+
+	Node* current = rhs.dummy.next;
+	Node* tail = &dummy;
+
+	while (current != nullptr)
+	{
+		tail->next = new Node;
+		tail = tail->next;
+
+		tail->value = current->value;
+		tail->next = nullptr;
+
+		current = current->next;
+	}
+}
