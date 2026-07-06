01e2057cf602a1e58ad629596f4e363e9599cc57
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-07-06 12:20:37 -0700

    outline for assignment 4 linked list push pop

---
+++ Assignment_4/Assignment_4/MyStack.h
@@ -0,0 +1,25 @@
+//  MyStack.h
+//  This contains all of the header files for the MyStack class.
+#ifndef MyStack_h
+#define MyStack_h
+
+#include "Node.h"
+
+using namespace std;
+
+class MyStack 
+{
+private:
+    Node dummy;
+public:
+    MyStack();
+    MyStack(const MyStack& rhs);
+    MyStack operator=(const MyStack& rhs);
+    void push(int v);
+    int pop();
+    int peek();
+    int size();
+    void printStack();
+    ~MyStack();
+};
+#endif
