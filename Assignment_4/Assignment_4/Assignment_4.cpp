7b5e8bea0d260654ba2abce48c5e012073d65503
Isaac Perez <Isaac.Perez.1@outlook.com>
2026-07-08 11:06:17 -0700

    used my stack functions in main

--- Assignment_4/Assignment_4/Assignment_4.cpp
+++ Assignment_4/Assignment_4/Assignment_4.cpp
@@ -2,10 +2,44 @@
 //
 
 #include <iostream>
+#include "MyStack.h"
+#include "Node.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    MyStack S;
+
+    S.push(1);
+    S.push(2);
+    S.push(3);
+    S.push(5);
+    S.push(6);
+    cout << "Testing push()" << endl;
+    S.printStack();
+
+    S.pop();
+    cout << "Testing pop()" << endl;
+    S.printStack();
+
+    cout << "Testing peek()" << endl;
+    cout << S.peek() << endl;
+
+    cout << "Testing size()" << endl;
+    cout << S.size() << endl;
+
+    S.pop();
+    S.pop();
+    S.pop();
+    S.pop();
+
+    cout << "Testing pop() over size limit" << endl;
+    cout << S.pop() << endl;
+
+    cout << "Testing peek() on empty stack" << endl;
+    cout << S.peek() << endl;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
